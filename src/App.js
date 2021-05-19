24b709af951849aa79e06d63c1206b3eabc94140
Sachin kaushik <sharmascahin066@gmail.com>
2021-05-20 00:48:02 +0530

    TweenMax and power 3 part 1 learned

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { TweenMax, Power3 } from "gsap";
+import { useRef, useEffect } from "react";
 
 function App() {
+  let logoItem = useRef(null);
+  let textItem=useRef(null)
+
+  const Gsap = () => {
+    TweenMax.to(logoItem, 0.2, { opacity: 1, y: -20, ease: Power3.easeOut,delay:.2 });
+    TweenMax.to(textItem, 0.2, { opacity: 1, y: -20, ease: Power3.easeOut,delay:.2 });
+  };
+
+  useEffect(() => {
+    Gsap();
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img
+          ref={(el) => {
+            logoItem = el;
+          }}
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
+        <p ref={(el)=>{textItem=el}}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
