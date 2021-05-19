970ff125799160f02d2b60c95e2281e19deb3efc
Sachin kaushik <sharmascahin066@gmail.com>
2021-05-20 01:27:46 +0530

    part 2 3 circle

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { TweenMax, Power3 } from "gsap";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect,useState } from "react";
 
 function App() {
-  let logoItem = useRef(null);
-  let textItem=useRef(null)
+  let app=useRef(null);
+  let circle = useRef(null);
+  let circleRed = useRef(null);
+  let circleBlue = useRef(null);
+  const[state,setState]=useState(true)
 
-  const Gsap = () => {
-    TweenMax.to(logoItem, 0.2, { opacity: 1, y: -20, ease: Power3.easeOut,delay:.2 });
-    TweenMax.to(textItem, 0.2, { opacity: 1, y: -20, ease: Power3.easeOut,delay:.2 });
+  const handleExpand=()=>{
+      TweenMax.to(circleRed,.5,{width:150,height:150,x:100,ease:Power3.easeOut})
+      setState(false)
+  }
+
+  const handleShrink=()=>{
+      TweenMax.to(circleRed,.5,{width:75,height:75,x:0,ease:Power3.easeOut})
+      setState(true)
+  }
+
+  const animation = () => {
+      TweenMax.to(app,0,{css:{visibility:"visible"}})
+    TweenMax.from(circle,.8,{opacity:0,x:40,ease:Power3})
+    TweenMax.from(circleRed,.8,{opacity:0,x:330,ease:Power3,delay:.2})
+    TweenMax.from(circleBlue,.8,{opacity:0,x:500,ease:Power3,delay:.4})
   };
 
   useEffect(() => {
-    Gsap();
-  });
+      animation()
+  }, []);
   return (
-    <div className="App">
+    <div className="App" ref={el=> app=el}>
       <header className="App-header">
-        <img
-          ref={(el) => {
-            logoItem = el;
-          }}
-          src={logo}
-          className="App-logo"
-          alt="logo"
-        />
-        <p ref={(el)=>{textItem=el}}>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="circle-container">
+          <div ref={(el) => (circle = el)} className="circle"></div>
+          <div onClick={state ? handleExpand :handleShrink} ref={(el) => (circleRed = el)} className="circle red"></div>
+          <div ref={(el) => (circleBlue = el)} className="circle blue"></div>
+        </div>
       </header>
     </div>
   );
